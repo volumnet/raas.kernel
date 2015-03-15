957507d2e3d839235c18ad9ec0dbc2233cab9a3a
Alex V. Surnin <info@volumnet.ru>
2015-03-15 20:51:17 +0500

    .

--- public/jquery.raas.js
+++ public/jquery.raas.js
@@ -97,6 +97,93 @@ jQuery(function($) {
         }
     };
 
+
+    $.fn.RAAS_autocompleter = function(method)
+    {
+        var $thisObj;
+        var $autotext;
+        var defaultParams = {
+            showInterval: 1000
+        };
+        var params;
+        var timeout_id = 0;
+        
+        var methods = {
+            getCompletion: function(data) {
+                var Set = data.Set;
+                var i;
+                $autotext.empty();
+                if (Set.length > 0) {
+                    for (i = 0; i < Set.length; i++) {
+                        var text = '<li>';
+                        text    += '  <a href="#" data-id="' + Set[i].id + '"';
+                        for (var key in Set[i]) {
+                            if ($.inArray(key, ['id', 'name', 'description', 'img']) == -1) {
+                                text += ' data-' + key + '="' + Set[i][key].toString() + '"';
+                            }
+                        }
+                        text += '>';
+                        if (Set[i].img) {
+                            text += '   <img src="' + Set[i].img + '" />';
+                        }
+                        text    += '    <span class="raas-autotext__name">' + Set[i].name + '</span>';
+                        text    += '    <span class="raas-autotext__description">' + Set[i].description + '</span>';
+                        text    += '  </a>';
+                        text    += '</li>';
+                        $autotext.append(text);
+                    }
+                    $autotext.show();
+                } else {
+                    methods.hideList();
+                }
+            },
+            textOnChange: function() {
+                $autotext.trigger('RAAS_autocompleter.change');
+                var text = $thisObj.val();
+                var url = params.url;
+                if (/\*/.test(url)) {
+                    var url = url.replace(/\*/, text);
+                } else {
+                    var url = url + text;
+                }
+                window.clearTimeout(timeout_id);
+                timeout_id = window.setTimeout(function() { $.getJSON(url, methods.getCompletion) }, params.showInterval);
+            },
+            onClick: function(e) {
+                $autotext.trigger('RAAS_autocompleter.click');
+                if (params.callback) {
+                    params.callback.apply(this, e);
+                }
+                $autotext.hide();
+                return false;
+            },
+            init: function(options) { 
+                $autotext.params = params = $.extend(defaultParams, options);
+                $thisObj.on('keyup', methods.textOnChange);
+                $('body').on('click', $autotext.hide);
+                $autotext.on('click', 'a', methods.onClick);
+            },
+        };
+
+        $thisObj = $(this);
+        $autotext = $thisObj.next('[data-role="raas-autotext"]');
+        if (!$autotext.length) {
+            $autotext = $('<ul class="raas-autotext" style="display: none" data-role="raas-autotext"></ul>')
+            $thisObj.after($autotext);
+        }
+        if ($autotext.params) {
+            $params = $autotext.params;
+        }
+    
+        // логика вызова метода
+        if ( methods[method] ) {
+            return methods[ method ].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        }
+    };
+
+
     $.fn.RAAS_menuTree = function(method)
     {
         var $thisObj;
@@ -257,6 +344,7 @@ jQuery(function($) {
                 var $repoElement = $repo.clone(true);
                 $repoElement.attr(params.repoElementChanges);
                 $repoContainer.append($repoElement);
+                $repoElement.trigger('RAAS_repo.add');
                 params.onAfterAdd.call($repoElement);
                 checkRequired();
                 $repoElement.RAASInitInputs();
@@ -275,6 +363,7 @@ jQuery(function($) {
                     $repoElement = $(params.repoElement);
                 }
                 params.onBeforeDelete.call($repoElement);
+                $repoElement.trigger('RAAS_repo.delete');
                 $repoElement.remove();
                 params.onAfterDelete.call($repoElement);
                 checkRequired();
