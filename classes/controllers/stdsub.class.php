375fa6ef11a65d31fa242acd2e8b3442990c73d9
Alex V. Surnin <info@volumnet.ru>
2016-12-15 11:47:05 +0500

    .

--- classes/controllers/stdsub.class.php
+++ classes/controllers/stdsub.class.php
@@ -1,31 +1,31 @@
 <?php
 /**
  * Файл стандартного подмодуля RAAS
- * 
+ *
  * Стандартный подмодуль объединяет типовые задачи, характерные для большинства SOME-объектов
  * @package RAAS
  * @version 4.1
  * @author Alex V. Surnin <info@volumnet.ru>
  * @copyright 2013, Volume Networks
- */       
+ */
 namespace RAAS;
 
 /**
  * Класс стандартного подмодуля RAAS
  * @package RAAS
- */       
+ */
 class StdSub extends Abstract_Sub_Controller
 {
     /**
      * Экземпляр класса
-     * @var \RAAS\StdSub     
-     */         
+     * @var \RAAS\StdSub
+     */
     protected static $instance;
-    
+
 
     /**
      * Конструктор класса
-     */         
+     */
     protected function init() {}
 
 
@@ -33,12 +33,12 @@ class StdSub extends Abstract_Sub_Controller
      * Перемещение объекта вверх по списку
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
      * @param mixed $where Дополнительное условие для поиска, например, общность родительских элементов
-     */         
+     */
     public static function move_up($data, $url = 'history::back', $conditionalBack = true, $filter = true, $where = null)
     {
         $items = self::getItems($data, $filter);
@@ -53,12 +53,12 @@ class StdSub extends Abstract_Sub_Controller
      * Перемещение объекта вниз по списку
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
      * @param mixed $where Дополнительное условие для поиска, например, общность родительских элементов
-     */         
+     */
     public static function move_down($data, $url = 'history::back', $conditionalBack = true, $filter = true, $where = null)
     {
         $items = self::getItems($data, $filter);
@@ -73,11 +73,11 @@ class StdSub extends Abstract_Sub_Controller
      * Смена видимости объекта
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
-     */         
+     */
     public static function chvis($data, $url = 'history::back', $conditionalBack = true, $filter = true)
     {
         $items = self::getItems($data, $filter);
@@ -93,11 +93,11 @@ class StdSub extends Abstract_Sub_Controller
      * Установка видимости объекта
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
-     */         
+     */
     public static function vis($data, $url = 'history::back', $conditionalBack = true, $filter = true)
     {
         $items = self::getItems($data, $filter);
@@ -113,11 +113,11 @@ class StdSub extends Abstract_Sub_Controller
      * Снятие видимости объекта
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
-     */         
+     */
     public static function invis($data, $url = 'history::back', $conditionalBack = true, $filter = true)
     {
         $items = self::getItems($data, $filter);
@@ -133,11 +133,11 @@ class StdSub extends Abstract_Sub_Controller
      * Удаление объекта
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
-     */         
+     */
     public static function delete($data, $url = 'history::back', $conditionalBack = true, $filter = true)
     {
         $items = self::getItems($data, $filter);
@@ -154,17 +154,17 @@ class StdSub extends Abstract_Sub_Controller
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param \SOME\SOME $Parent новый родительский объект (должен быть того же типа)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
-     */         
+     */
     public static function move($data, \SOME\SOME $Parent, $url = 'history::back', $conditionalBack = true, $filter = true)
     {
         $items = self::getItems($data, $filter);
         foreach ($items as $Item) {
             if (
-                (get_class($Item) != get_class($Parent)) || 
+                (get_class($Item) != get_class($Parent)) ||
                 !in_array($Parent->id, array_merge(array((int)$Item->id, (int)$Item->pid), (array)$Item->all_children_ids))
             ) {
                 $Item->pid = (int)$Parent->id;
@@ -179,11 +179,11 @@ class StdSub extends Abstract_Sub_Controller
      * Произвольное именованное действие с объектом
      * @param mixed $data Данные об объектах (обработка с помощью метода getItems - см. описание там)
      * @param string url URL для перехода после выполнения действия
-     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее 
+     * @param bool $conditionalBack если установлен в TRUE, функция будет проверять наличие переменной $_GET['back'] и при обнаружении ее
      *                              автоматически заменять параметр $url на 'history:back'
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
-     */         
+     */
     public static function __callStatic($f, $args)
     {
         if (isset($args[0])) {
@@ -195,7 +195,7 @@ class StdSub extends Abstract_Sub_Controller
         $conditionalBack = isset($args[2]) ? (bool)$args[2] : true;
         $filter = isset($args[3]) ? $args[3] : true;
         $arguments = (count($args) > 4) ? array_slice($args, 4) : array();
-        
+
         $items = self::getItems($data, $filter);
         foreach ($items as $Item) {
             call_user_func_array(array($Item, $f), $arguments);
@@ -207,11 +207,11 @@ class StdSub extends Abstract_Sub_Controller
     /**
      * Получение объектов из входных данных
      * @param array[\SOME\SOME]|\SOME\SOME|object $data Входные данные
-     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE, 
+     * @param callable|bool $filter дополнительное условие - для выполнения фактического действия должен быть TRUE,
      *                              либо (если callable) возвращать TRUE от объекта $Item
      * @return array[\SOME\SOME|object] массив объектов для обработки
      */
-    public function getItems($data, $filter = true)
+    public static function getItems($data, $filter = true)
     {
         if (!$filter) {
             return array();
