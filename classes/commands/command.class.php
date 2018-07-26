b31ab56d4b0527caef3e53d932517f6ce8b93632
Alex V. Surnin <info@volumnet.ru>
2018-07-26 14:04:43 +0500

    .

--- classes/commands/command.class.php
+++ classes/commands/command.class.php
@@ -10,6 +10,22 @@ namespace RAAS;
 abstract class Command
 {
     /**
+     * Контроллер, вызвавший команду
+     * @var Abstract_Controller_Cron|null
+     */
+    protected $controller = null;
+
+
+    /**
+     * Конструктор класса
+     * @param Abstract_Controller_Cron|null $controller Контроллер, вызвавший команду
+     */
+    public function __construct(Abstract_Controller_Cron $controller = null)
+    {
+        $this->controller = $controller;
+    }
+
+    /**
      * Выполнение команды
      */
     abstract public function process();
