6e8a415ceb7297e8205e34c74f798ddaf78615ee
Alex V. Surnin <info@volumnet.ru>
2018-10-30 17:11:48 +0500

    .

--- public/jquery.raas.js
+++ public/jquery.raas.js
@@ -1,4 +1,28 @@
 jQuery(function($) {
+    function formatPrice(price)
+    {
+        var pSign = (price < 0) ? '-' : '';
+        price = Math.abs(price);
+        var pR = Math.round((parseFloat(price) - parseInt(price)) * 100);
+        var pS = parseInt(price).toString();
+        var pT = '';
+        var i;
+
+        for (i = 0; i < pS.length; i++) {
+            var j = pS.length - i - 1;
+            pT = ((i % 3 == 2) && (j > 0) ? ' ' : '') + pS.substr(j, 1) + pT;
+        }
+        if (pR > 0) {
+            pR = pR.toString();
+            if (pR.length < 2) {
+                pR = '0' + pR;
+            }
+            pT += ',' + pR;
+        }
+        pT = pSign + pT;
+        return pT;
+    }
+
     $.fn.RAAS_tree = function(method)
     {
         var $thisObj;
