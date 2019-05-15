793fb051bfa7359efe6f9b4156b1eb970fffbc3b
Alex V. Surnin <info@volumnet.ru>
2019-05-15 12:07:01 +0500

    .

---
+++ classes/commands/mailtestcommand.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Файл класса команды проверки отправки почты
+ */
+namespace RAAS;
+
+/**
+ * Класс комманды проверки отправки почты
+ */
+class MailTestCommand extends Command
+{
+    /**
+     * Отправляет тестовое письмо
+     * @param string $email Адрес, на который отправляем
+     * @param string $fromEmail Обратный адрес
+     */
+    public function process($email = null, $fromEmail = null)
+    {
+        $subject = 'Test';
+        $message = '<p>This is a test message. Please do not reply.</p>';
+        $from =
+        Application::i()->sendmail(
+            [$email],
+            $subject,
+            $message,
+            $fromEmail,
+            $fromEmail,
+            true
+        );
+    }
+}
